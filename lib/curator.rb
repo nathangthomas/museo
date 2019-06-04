b9fddada153a3fe6f90fdbfe736a7e4ad8fb1f7b
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-04 10:45:20 -0600

    Add curator class

---
+++ lib/curator.rb
@@ -0,0 +1,25 @@
+class Curator
+  attr_reader :artists, :photographs
+
+  def initialize
+    @artists = []
+    @photographs = []
+  end
+
+  def add_photograph(photo)
+    @photographs << Photograph.new(photo)
+  end
+
+  def add_artist(artist)
+    @artists << Artist.new(artist)
+  end
+
+  def find_artist_by_id(id)
+    @artists.find{|artist| artist.id == id}
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find{|photo| photo.id == id}
+  end
+  
+end
